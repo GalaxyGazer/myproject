2879a770f5ca4b61844f57bf1ce03c575b0e10df
GalaxyGazer <messier8182@gmail.com>
2017-05-05 13:03:14 -0400

    First commit of HelloWorld file

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        
+	HelloWorld helloWorld = new HelloWorld();
+	helloWorld.test();
+    }
+
+    private void test() {
+        System.out.println("Hello, WOrld!");
+    }
+}
